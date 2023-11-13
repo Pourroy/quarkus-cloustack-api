b91ceef976fed7c8a2b485148fa38951fbd66974
ruan.afonso <ruan.afonso@locaweb.com.br>
2023-11-13 09:15:51 -0300

    query for databases

--- src/main/java/app/template/api/ControlApi.java
+++ src/main/java/app/template/api/ControlApi.java
@@ -1,4 +1,25 @@
 package app.template.api;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.stream.Stream;
+
+
+@Path("/controls")
 public class ControlApi {
+  @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Stream yourCustomQuery() {
+      return entityManager.createNativeQuery("SELECT * FROM controls").getResultStream();
+    }
 }
+
